e0712410def41064e77e7ff05b49e2a5cd18af18
hfarazul <h.farazul@gmail.com>
2023-07-18 02:11:15 +0200

    frontend

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Prompt from './prompt'; // <-- Add this line
+
+function App() {
+  return (
+    <div className="App">
+      <Prompt />
+    </div>
+  );
+}
+
+export default App;
